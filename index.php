7c5a3a49916968c026001032d4d613e60939f9e8
Zandrix4 <100114369+Zandrix4@users.noreply.github.com>
2022-03-13 21:55:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
   <h1>welcome to My Website! I am Zandrix Pongos!</h1>
   <ul style="list-style-type:disc">
     <li><a href="index.php">HOME</a></li>
-    <li><a href="/LT2/index.html">LEARNING TASK 2</a></l>
-    <li><a href="/LT3/index.html">LEARNING TASK 3</a></l>
+    <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+    <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
   </ul>
 </body>
 </html>
