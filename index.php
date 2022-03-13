bc1a72b7fe447782f73b15c12b2a6f3797a56a88
Zandrix4 <100114369+Zandrix4@users.noreply.github.com>
2022-03-13 21:46:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <title>My Home Page</title>
   </head>
   <body
-  <h1>welcome to My Website! I am {your name}!</h1>
+  <h1>welcome to My Website! I am Zandrix Pongos!</h1>
   <ul>
     <li><a href="index. php">HOME</a></li>
     <li><a href="/LT2/index.html">LEARNING TASK 2</a></li»
+    <li><a href="/LT3/index.html">LEARNING TASK 2</a></li»
   </ul>
   </body
 </html>
